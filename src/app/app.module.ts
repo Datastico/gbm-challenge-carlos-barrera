7451f595512e08288e593f9c4a641975eaf54e81
Carlos Barrera <karlosss@MacBook-Pro-de-Carlos.local>
2018-11-19 11:14:57 -0600

    Second commit: Create component skelletons and configure header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { GraphComponent } from './components/graph/graph.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    GraphComponent,
+    IntroComponent,
+    LoginComponent,
+    FooterComponent,
+    NotFoundComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
